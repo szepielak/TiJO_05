9f17af9cc1d1bb665b8998a7339e87cee9e9a76e
aniaw <ania_w@outolook.com>
2016-03-20 16:25:37 +0100

    add assignToSwimmingCourse function

--- app/app.js
+++ app/app.js
@@ -1,7 +1,26 @@
 (function () {
     'use strict';
 
-    window.app = {}
+    window.app = {
+        assignToSwimmingCourse: function (name, birthDate) {
+            var course;
+            var age = this.calculateAge(birthDate);
+
+            if (age >= 3 && age <= 12) {
+                course = {name: name, age: age, course: 'kids'};
+            }
+            else if (age > 12 && age < 18) {
+                course = {name: name, age: age, course: 'teens'};
+            }
+            else if (age >= 18) {
+                course = {name: name, age: age, course: 'adults'};
+            }
+            else {
+                throw new TypeError('Wrong data or too young!');
+            }
+            return course;
+        }
+    }
 })();
 
 
