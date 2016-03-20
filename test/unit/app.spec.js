95a4b0f635006146fc0b1247e3caf021e018d128
aniaw <ania_w@outolook.com>
2016-03-20 16:17:49 +0100

    improve CarPortal tests

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -32,16 +32,22 @@ describe('app', function () {
     var carPortal;
     describe('CarPortal', function () {
         carPortal = new app.CarPortal();
-
+        afterEach(function () {
+            carPortal.logout();
+        });
         it('should set username to john and password to 12345 when login is call', function () {
             carPortal.login('john', '12345');
             expect(carPortal.credentials).toEqual({username: 'john', password: '12345'});
         });
+        it('should clean credentials after each it', function () {
+            expect(carPortal.credentials).toEqual({});
+        });
         it('should set username to maria and password to qwerty when login is call', function () {
-            console.log('credential');
-            console.log(carPortal.credentials);
             carPortal.login('maria', 'qwerty');
             expect(carPortal.credentials).toEqual({username: 'maria', password: 'qwerty'});
+        });
+        it('should clean credentials after each it', function () {
+            expect(carPortal.credentials).toEqual({});
         });
     });
 });
