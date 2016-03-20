0ceccbc59d1386136d49a4cc5501def0f7ce1e6a
aniaw <ania_w@outolook.com>
2016-03-20 16:01:46 +0100

    add Car test

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -3,6 +3,27 @@ describe('app', function () {
 
     var app = window.app;
 
+    var car;
+    describe('Car', function () {
+        describe('property', function () {
+            car = new app.Car('Audi', 1998);
 
+            it('should defined model', function () {
+                expect(car.model).toBeDefined();
+            });
+            it('should defined year', function () {
+                expect(car.year).toBeDefined();
+            });
+        });
+        describe('value', function () {
+            car = new app.Car('Audi', 1998);
+
+            it('should be set to Audi on model property', function () {
+                expect(car.model).toBe('Audi');
+            });
+            it('should be set to 1998 on year property', function () {
+                expect(car.year).toBe(1998);
+            });
+        });
+    });
 });
-
