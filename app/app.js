70e5f644999221af19b4e9869c3feb5519cb6c5b
aniaw <ania_w@outolook.com>
2016-03-21 19:38:07 +0100

    add spy-exercise

--- app/app.js
+++ app/app.js
@@ -1,7 +1,43 @@
 (function () {
     'use strict';
 
-    window.app = {}
+    window.app = {
+        isPalindrome: function (str) {
+            var strTemp = str.toLowerCase(),
+                strLength = strTemp.length;
+            if (str === '') {
+                return false;
+            }
+            var halfLength = (strLength % 2 === 0) ? (strLength / 2) : ((strLength - 1) / 2);
+            for (var i = 0; i < halfLength; i++) {
+                if (strTemp[i] !== strTemp.slice(-1 - i)[0]) {
+                    return false;
+                }
+            }
+            return true;
+        },
+
+        vowelCount: function (str) {
+            var vowelList = 'aeiouyAEIOUY',
+                vovCount = 0;
+            for (var i = 0, strLength = str.length; i < strLength; i++) {
+                if (vowelList.indexOf(str[i]) !== -1) {
+                    vovCount++;
+                }
+            }
+            return vovCount;
+        },
+        generateMessage: function (text) {
+            var palindrome = this.isPalindrome(text);
+            var vowel = this.vowelCount(text);
+            if (text.length > 0) {
+                return {vowel: vowel, palindrome: palindrome};
+            } else {
+                throw new Error('Empty string!');
+            }
+        }
+    };
+
 })();
 
 
--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -3,6 +3,54 @@ describe('app', function () {
 
     var app = window.app;
 
+    describe('generateMessage', function () {
 
+    });
+
+    describe('isPalindrome', function () {
+
+        describe('toHaveBeenCalled', function () {
+
+        });
+
+        describe('and.callThrough', function () {
+
+        });
+
+        describe('and.returnValue', function () {
+
+        });
+
+        describe('and.callFake', function () {
+
+        });
+
+        describe('calls.count()', function () {
+
+        });
+    });
+
+    describe('vowelCount', function () {
+
+        describe('toHaveBeenCalled', function () {
+
+        });
+
+        describe('and.callThrough', function () {
+
+        });
+
+        describe('and.returnValue', function () {
+
+        });
+
+        describe('and.callFake', function () {
+
+        });
+
+        describe('calls.count()', function () {
+
+        });
+    });
 });
 
